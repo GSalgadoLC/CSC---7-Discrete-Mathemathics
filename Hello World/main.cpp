e56840a5c24d4c759821305de797b5899fdd2d3e
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2023-02-23 23:59:37 -0800

    Add files via upload

---
+++ Hello World/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Gustavo Salgado
+ *
+ * Created on February 23, 2023
+ * Purpose: Demonstrate functionality of NetBeans by creating a simple Hello World
+ */
+
+//System Libraries
+#include <iostream> 
+#include <cstdlib>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Science, Math, Conversions,Dimensions
+
+//Function Prototypes
+
+int main(int argc, char** argv) {
+    //Set random number seed
+    //Declare variables
+    string hello;
+    //Initialize variables
+    hello = "Hello, World!";
+    //Map/Process the Inputs -> Outputs 
+    //Display Inputs/Outputs
+    cout<<hello<<endl;
+    //Clean up memory and files
+    //Exit the program
+    return 0;
+}
+
